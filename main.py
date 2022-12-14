1aee1a91dd1129688b20f834bf51fb2900ca0487
Ludovic Andrianoff <landrianoff2@gmail.com>
2022-12-14 17:57:25 +0100

    Added bruteforce method

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+from itertools import combinations
+import csv
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+start_time = time.time()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    stocks_list = read_csv()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f"\nProcessing {len(stocks_list)} shares:")
+
+    best_combo = set_combos(stocks_list)
+
+    print(best_combo)
+    print(time.time() - start_time, "seconds")
+
+
+def read_csv():
+    with open("data/test.csv") as csvfile:
+        stocks_file = csv.reader(csvfile, delimiter=',')
+
+        stocks = []
+        for row in stocks_file:
+            stocks.append(
+                (row[0], float(row[1]), float(row[2]))
+            )
+        return stocks
+
+
+def set_combos(stocks_list):
+    profit = 0
+    best_combo = []
+    best_cost = 0
+
+    for i in range(len(stocks_list)):
+        combos = combinations(stocks_list, i + 1)
+
+        for combo in combos:
+            total_cost = sum([stock[1] for stock in combo])
+
+            if total_cost <= 500:
+                total_profit = sum([stock[1] * stock[2] / 100 for stock in combo])
+
+                if total_profit > profit:
+                    profit = total_profit
+                    best_combo = combo
+                    best_cost = total_cost
+
+    return best_combo, profit, best_cost
+
+
+main()
